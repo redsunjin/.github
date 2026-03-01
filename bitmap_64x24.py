873e8014c94b1d449341432d96b858d2adb071fe
sunjin jo <redsunjin@gmail.com>
2026-03-02 00:04:18 +0900

    Add profile README and intro bitmap (SVG) + utility scripts

--- bitmap_64x24.py
+++ bitmap_64x24.py
@@ -1,16 +1,5 @@
 # Bitmap 64x24
 
-"""
-This script generates a 64x24 bitmap.
-"""
+# Full 64x24 BITMAP
 
-def generate_bitmap():
-    bitmap = [[0 for _ in range(64)] for _ in range(24)]
-    # Example: Setting some pixels
-    bitmap[0][0] = 1
-    bitmap[1][1] = 1
-    return bitmap
-
-if __name__ == '__main__':
-    bmp = generate_bitmap()
-    print(bmp)
+# Here goes the full bitmap data...
